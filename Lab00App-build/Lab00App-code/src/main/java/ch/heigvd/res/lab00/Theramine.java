1a727de8fbd4161c6c1ab3bec4384539e840238c
kevinmoreirad <kevin.moreiradacosta@heig-vd.ch>
2017-03-10 14:20:00 +0100

    Create Theramine.java

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Theramine.java
@@ -0,0 +1,20 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kevin moreira
+ */
+public class Theramine implements IInstrument{
+
+
+    @Override
+    public String play() {
+        return "bzzz";
+    }
+}
